b90ba0625eaf8bbb884d5937890c60433eaaeb5d
Alessandro Resta <alessandro.resta@gmail.com>
2017-02-12 16:20:34 +0000

    Add unit test for handlerNotFound function

--- src/server/handlers_test.go
+++ src/server/handlers_test.go
@@ -7,6 +7,26 @@ import (
 	"testing"
 )
 
+func TestHandlerNotFound(t *testing.T) {
+
+	req, err := http.NewRequest("Get", "/foo", nil)
+	if err != nil {
+		t.Error("Failed to prepare request: ", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(handlerNotFound)
+	handler.ServeHTTP(rr, req)
+
+	expectedResponseCode := 404
+	responseCode := rr.Code
+
+	if expectedResponseCode != rr.Code {
+		t.Errorf("For '/foo' endpoint: expecting response code %d. Received: %d", expectedResponseCode, responseCode)
+	}
+}
+
 // Test "ping" endpoint
 func TestHandlerPing(t *testing.T) {
 
