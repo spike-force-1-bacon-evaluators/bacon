b085eb90847a9b85566f3eba3b502a72f6754abb
Alessandro Resta <alessandro.resta@gmail.com>
2017-02-12 16:21:18 +0000

    Implement handler for 404

--- src/server/handlers.go
+++ src/server/handlers.go
@@ -10,11 +10,17 @@ import (
 // Handlers its all here...
 func handlers() *mux.Router {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(handlerNotFound)
 	r.HandleFunc("/ping", handlerPing)
 	return r
 }
 
 // For testing only
+// Page not found handler
+func handlerNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 - Page Not Found"))
+}
 func handlerPing(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "PONG")
 }
