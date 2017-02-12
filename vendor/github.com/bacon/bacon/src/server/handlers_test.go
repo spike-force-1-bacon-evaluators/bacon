80f826464cca23815d972e01848f186ce48c39cc
Alessandro Resta <alessandro.resta@gmail.com>
2017-02-12 16:34:50 +0000

    Update vendor

---
+++ vendor/github.com/bacon/bacon/src/server/handlers_test.go
@@ -0,0 +1,78 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// Test "/" endpoint
+func TestHandlerRoot(t *testing.T) {
+
+	// Prepare GET request to "/" endpoint
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Error("Failed to prepare request: ", err)
+	}
+
+	// record the response
+	rr := httptest.NewRecorder()
+
+	// Start test server prepared to receive request on 'ping' endpoint
+	handler := http.HandlerFunc(handlerPing)
+	handler.ServeHTTP(rr, req)
+
+	expectedResponseCode := 200
+	responseCode := rr.Code
+
+	// Compare response body
+	if expectedResponseCode != responseCode {
+		t.Errorf("For '/' endpoint: expecting response code %d. Received: %d", expectedResponseCode, responseCode)
+	}
+}
+
+func TestHandlerNotFound(t *testing.T) {
+
+	req, err := http.NewRequest("Get", "/foo", nil)
+	if err != nil {
+		t.Error("Failed to prepare request: ", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(handlerNotFound)
+	handler.ServeHTTP(rr, req)
+
+	expectedResponseCode := 404
+	responseCode := rr.Code
+
+	if expectedResponseCode != rr.Code {
+		t.Errorf("For '/foo' endpoint: expecting response code %d. Received: %d", expectedResponseCode, responseCode)
+	}
+}
+
+// Test "ping" endpoint
+func TestHandlerPing(t *testing.T) {
+
+	// Prepare GET request to ping endpoint
+	req, err := http.NewRequest("GET", "/ping", nil)
+	if err != nil {
+		log.Fatal("Failed to prepare request: ", err)
+	}
+
+	// record the response
+	rr := httptest.NewRecorder()
+
+	// Start test server prepared to receive request on 'ping' endpoint
+	handler := http.HandlerFunc(handlerPing)
+	handler.ServeHTTP(rr, req)
+
+	expectedResponse := "PONG"
+	responseBody := rr.Body.String()
+
+	// Compare response body
+	if responseBody != expectedResponse {
+		t.Errorf("For 'ping' endpoint: expecting response %s. Received: %s", expectedResponse, responseBody)
+	}
+}
---
+++ vendor/github.com/bacon/bacon/src/server/indextmpl_test.go
@@ -0,0 +1,98 @@
+package server
+
+import "testing"
+
+type expected struct {
+	indicator *Indicator
+	variation int
+}
+
+type testcases struct {
+	input    *Restaurant
+	expected *expected
+}
+
+// Test variation modulo
+func TestNewIndicatorVariation(t *testing.T) {
+
+	var testCases = []*testcases{
+		{
+			&Restaurant{Variation: 1},
+			&expected{variation: 1},
+		},
+		{
+			&Restaurant{Variation: 0},
+			&expected{variation: 0},
+		},
+		{
+			&Restaurant{Variation: -1},
+			&expected{variation: 1},
+		},
+	}
+
+	for _, test := range testCases {
+		_ = test.input.newIndicator()
+
+		if test.input.Variation != test.expected.variation {
+			t.Errorf("For variation: %d. Expected: %d. Received: %d",
+				test.input.Variation, test.expected.variation, test.input.Variation)
+		}
+	}
+}
+
+// Test indicator direction for variation values
+func TestNewIndicatorDirection(t *testing.T) {
+
+	var testCases = []*testcases{
+		{
+			&Restaurant{Variation: 1},
+			&expected{indicator: &Indicator{Direction: "⇧"}},
+		},
+		{
+			&Restaurant{Variation: 0},
+			&expected{indicator: &Indicator{Direction: "▭"}},
+		},
+		{
+			&Restaurant{Variation: -1},
+			&expected{indicator: &Indicator{Direction: "⇩"}},
+		},
+	}
+
+	for _, test := range testCases {
+		observed := test.input.newIndicator()
+
+		if observed.Direction != test.expected.indicator.Direction {
+			t.Errorf("For variation: %d. Expected: %s. Received: %s",
+				test.input.Variation, test.expected.indicator.Direction, observed.Direction)
+		}
+
+	}
+}
+
+// Test indicator colour for variation values
+func TestNewIndicatorColour(t *testing.T) {
+
+	var testCases = []*testcases{
+		{
+			&Restaurant{Variation: 1},
+			&expected{indicator: &Indicator{Colour: "green"}},
+		},
+		{
+			&Restaurant{Variation: 0},
+			&expected{indicator: &Indicator{}},
+		},
+		{
+			&Restaurant{Variation: -1},
+			&expected{indicator: &Indicator{Colour: "red"}},
+		},
+	}
+
+	for _, test := range testCases {
+		observed := test.input.newIndicator()
+
+		if observed.Colour != test.expected.indicator.Colour {
+			t.Errorf("For colour: %s. Expected: %s. Received: %s",
+				test.input.Indicator.Colour, test.input.Indicator.Colour, observed.Colour)
+		}
+	}
+}
