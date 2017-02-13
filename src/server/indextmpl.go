a243bc1c33f489b61de41c60a69cea4fa6c38390
Alessandro Resta <alessandro.resta@gmail.com>
2017-02-13 21:28:10 +0000

    Remove mock data and parse restaurants from neo4bacon

--- src/server/indextmpl.go
+++ src/server/indextmpl.go
@@ -4,6 +4,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/bacon/bacon/src/neo4bacon"
 )
 
 // Meta information
@@ -33,29 +36,23 @@ type Index struct {
 	Restaurants []*Restaurant
 }
 
+var restaurantList []*neo4bacon.Restaurant
 
 // Create and execute index template
 // TODO: change this function accordingly to values returned from Neo4j,
 // remove mock data, and write test
 func indextmpl(w http.ResponseWriter) {
 
-	restaurants := []*Restaurant{
-		&Restaurant{
-			Position:  1,
-			Name:      "The Restaurant at the End of the Universe",
-			Variation: 2},
-		&Restaurant{
-			Position:  2,
-			Name:      "Barriga Estufada",
-			Variation: 1},
-		&Restaurant{
-			Position:  3,
-			Name:      "PratoFeito",
-			Variation: -2},
-		&Restaurant{
-			Position:  4,
-			Name:      "Só Maminha",
-			Variation: 0},
+	restaurantList = neo4bacon.Run()
+	var restaurants []*Restaurant
+
+	for _, r := range restaurantList {
+		restaurant := &Restaurant{
+			Name:      r.Name,
+			Position:  r.Newpos,
+			Variation: r.Change,
+		}
+		restaurants = append(restaurants, restaurant)
 	}
 
 	for _, r := range restaurants {
@@ -82,20 +79,45 @@ func indextmpl(w http.ResponseWriter) {
 // This method assigns a symbol (arrow or rectangle) to
 // a new Indicator accordingly the Variation value.
 func (r *Restaurant) newIndicator() *Indicator {
-	if r.Variation > 0 {
+
+	if r.Variation != "N/A" {
+
+		variation, err := strconv.Atoi(r.Variation)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if variation > 0 {
+			return &Indicator{
+				Direction: "⇧",
+				Colour:    "green",
+			}
+		}
+
+		if variation < 0 {
+			variationMod, err := strconv.Atoi(r.Variation)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			variationMod = -variationMod
+			r.Variation = strconv.Itoa(variationMod)
+
+			return &Indicator{
+				Direction: "⇩",
+				Colour:    "red",
+			}
+		}
 		return &Indicator{
-			Direction: "⇧",
-			Colour:    "green",
+			Direction: "▭",
 		}
 	}
-	if r.Variation < 0 {
-		r.Variation = -r.Variation
-		return &Indicator{
-			Direction: "⇩",
-			Colour:    "red",
-		}
+
+	ind := &Indicator{
+		Direction: "",
+		Colour:    "black",
 	}
-	return &Indicator{
-		Direction: "▭",
-	}
+	r.Indicator = ind
+	return r.Indicator
 }
